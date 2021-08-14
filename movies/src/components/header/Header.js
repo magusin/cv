df2bce727ad551fe7b6b5b91a5c62d0ab0b6e440
magusin <fanatsy4@gmail.com>
2021-08-14 20:26:37 +0200

    ajout projet NALA

--- movies/src/components/header/Header.js
+++ movies/src/components/header/Header.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Header extends Component {
-
   render() {
     return (
-        <header className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="/">AlloMovie</a>
-          <button className="navbar-toggler">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse">
+      <header className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/movies/build/">
+          AlloMovie
+        </a>
+        <button className="navbar-toggler">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="/">Home</a>
+              <a className="nav-link" href="/movies/build/">
+                Home
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">Favoris</a>
+              <a className="nav-link" href="/movies/build/">
+                Favoris
+              </a>
             </li>
           </ul>
         </div>
       </header>
     );
   }
-
 }
